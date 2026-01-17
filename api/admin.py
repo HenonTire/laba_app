f407a51a23b743b34b5d11d60a99ee1db6c61144
Henon <henontireso@gmail.com>
2026-01-17 23:55:44 +0300

    add models

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Tasks, Projects, Plans
+admin.site.register(Tasks)
+admin.site.register(Projects)
+admin.site.register(Plans)
